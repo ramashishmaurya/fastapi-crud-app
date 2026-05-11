660d57e797c9cf51f8a8677893e28ea1021b8d07
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-05-11 21:26:20 +0530

    change some code

--- services.py
+++ services.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import session
 from schemas import CreatBook
 
 def create_book(db :session , data:CreatBook):
-    book_instance = Book(**data.model_dump())
+    book_instance = Book(**data.model_dump())  
     db.add(book_instance)
     db.commit()
     db.refresh(book_instance)
     return book_instance
 
-def get_books(db:session):
+def get_books(db:session): 
     return db.query(Book).all()
 
 def get_book(db:session , book_id:int):
